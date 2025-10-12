44271939a7a9ed22591c77ac3bbdc39b2260e1f6
DanPeled <danpeled31@gmail.com>
2025-10-12 10:19:16 +0300

    converted to using estimationmode instead of boolean for estimation mode (mt1 , mt2)

--- yall/java/limelight/networktables/LimelightPoseEstimator.java
+++ yall/java/limelight/networktables/LimelightPoseEstimator.java
@@ -1,6 +1,5 @@
 package limelight.networktables;
 
-
 import static limelight.networktables.LimelightUtils.toPose3D;
 
 import edu.wpi.first.math.geometry.Pose3d;
@@ -12,33 +11,36 @@ import limelight.Limelight;
 /**
  * Pose estimator for {@link Limelight}.
  */
-public class LimelightPoseEstimator
-{
+public class LimelightPoseEstimator {
+  public static enum EstimationMode {
+    MEGATAG1,
+    MEGATAG2
+  }
 
   /**
    * {@link Limelight} name to use.
    */
-  private final Limelight        limelight;
+  private final Limelight limelight;
   /**
    * Use MegaTag2 for the {@link PoseEstimate}.
    */
-  private final boolean          megatag2;
+  private final EstimationMode estimationMode;
   /**
    * Old botpose from megatag1
    */
   @Deprecated
-  private       DoubleArrayEntry botpose;
+  private DoubleArrayEntry botpose;
 
   /**
-   * Construct {@link LimelightPoseEstimator} which fetches data from NetworkTables
+   * Construct {@link LimelightPoseEstimator} which fetches data from
+   * NetworkTables
    *
    * @param camera   {@link Limelight} to use.
    * @param megatag2 MegaTag2 decoding.
    */
-  public LimelightPoseEstimator(Limelight camera, boolean megatag2)
-  {
+  public LimelightPoseEstimator(Limelight camera, EstimationMode estimationMode) {
     limelight = camera;
-    this.megatag2 = megatag2;
+    this.estimationMode = estimationMode;
     botpose = limelight.getNTTable().getDoubleArrayTopic("botpose").getEntry(new double[0]);
   }
 
@@ -48,34 +50,31 @@ public class LimelightPoseEstimator
    * @return {@link Pose3d} of the MegaTag1 pose estimate for the bot pose.
    */
   @Deprecated
-  public Pose3d getBotPose()
-  {
+  public Pose3d getBotPose() {
     return toPose3D(botpose.get());
   }
 
   /**
    * Get the {@link PoseEstimate} corresponding with your alliance color.
    * <p>
-   * Alliance comes from DriverStation. If simulation, then the sim GUI must not be in "disconnected" mode.
+   * Alliance comes from DriverStation. If simulation, then the sim GUI must not
+   * be in "disconnected" mode.
    *
    * @return {@link Optional} of {@link PoseEstimate} of your given alliance.
    */
-  public Optional<PoseEstimate> getAlliancePoseEstimate()
-  {
-    if (DriverStation.getAlliance().isEmpty())
-    {
+  public Optional<PoseEstimate> getAlliancePoseEstimate() {
+    if (DriverStation.getAlliance().isEmpty()) {
       return Optional.empty();
     }
 
-    switch (DriverStation.getAlliance().get())
-    {
-      case Red ->
-      {
-        return megatag2 ? BotPose.RED_MEGATAG2.get(limelight) : BotPose.RED.get(limelight);
+    switch (DriverStation.getAlliance().get()) {
+      case Red -> {
+        return estimationMode == EstimationMode.MEGATAG2 ? BotPose.RED_MEGATAG2.get(limelight)
+            : BotPose.RED.get(limelight);
       }
-      case Blue ->
-      {
-        return megatag2 ? BotPose.BLUE_MEGATAG2.get(limelight) : BotPose.BLUE.get(limelight);
+      case Blue -> {
+        return estimationMode == EstimationMode.MEGATAG2 ? BotPose.BLUE_MEGATAG2.get(limelight)
+            : BotPose.BLUE.get(limelight);
       }
     }
     return Optional.empty();
@@ -86,46 +85,48 @@ public class LimelightPoseEstimator
    *
    * @return {@link Optional} of {@link PoseEstimate} for blue-origin based poses.
    */
-  public Optional<PoseEstimate> getPoseEstimate()
-  {
-    return megatag2 ? BotPose.BLUE_MEGATAG2.get(limelight) : BotPose.BLUE.get(limelight);
+  public Optional<PoseEstimate> getPoseEstimate() {
+    return estimationMode == EstimationMode.MEGATAG2 ? BotPose.BLUE_MEGATAG2.get(limelight)
+        : BotPose.BLUE.get(limelight);
   }
-
 
   /**
    * BotPose enum for easier decoding.
    */
-  public enum BotPose
-  {
+  public enum BotPose {
     /**
-     * (Not Recommended) The robot's pose in the WPILib Red Alliance Coordinate System.
+     * (Not Recommended) The robot's pose in the WPILib Red Alliance Coordinate
+     * System.
      */
     RED("botpose_wpired", false),
     /**
-     * (Not Recommended) The robot's pose in the WPILib Red Alliance Coordinate System with MegaTag2.
+     * (Not Recommended) The robot's pose in the WPILib Red Alliance Coordinate
+     * System with MegaTag2.
      */
     RED_MEGATAG2("botpose_orb_wpired", true),
     /**
-     * (Recommended) The robot's 3D pose in the WPILib Blue Alliance Coordinate System.
+     * (Recommended) The robot's 3D pose in the WPILib Blue Alliance Coordinate
+     * System.
      */
     BLUE("botpose_wpiblue", false),
     /**
-     * (Recommended) The robot's 3D pose in the WPILib Blue Alliance Coordinate System with MegaTag2.
+     * (Recommended) The robot's 3D pose in the WPILib Blue Alliance Coordinate
+     * System with MegaTag2.
      */
     BLUE_MEGATAG2("botpose_orb_wpiblue", true);
 
     /**
      * {@link Limelight} botpose entry name.
      */
-    private final String                 entry;
+    private final String entry;
     /**
      * Is megatag2 reading?
      */
-    private final boolean                isMegaTag2;
+    private final boolean isMegaTag2;
     /**
      * Current {@link PoseEstimate}
      */
-    private       Optional<PoseEstimate> poseEstimate;
+    private Optional<PoseEstimate> poseEstimate;
 
     /**
      * Create {@link BotPose} enum with given entry names and megatag2 state.
@@ -133,8 +134,7 @@ public class LimelightPoseEstimator
      * @param entryName Bot Pose entry name for {@link Limelight}
      * @param megatag2  MegaTag2 reading.
      */
-    BotPose(String entryName, boolean megatag2)
-    {
+    BotPose(String entryName, boolean megatag2) {
       entry = entryName;
       isMegaTag2 = megatag2;
       poseEstimate = Optional.empty();
@@ -146,18 +146,14 @@ public class LimelightPoseEstimator
      * @param camera {@link Limelight} to use.
      * @return Current {@link PoseEstimate}.
      */
-    public Optional<PoseEstimate> get(Limelight camera)
-    {
-      if (poseEstimate.isEmpty())
-      {
+    public Optional<PoseEstimate> get(Limelight camera) {
+      if (poseEstimate.isEmpty()) {
         PoseEstimate estimate = new PoseEstimate(camera, entry, isMegaTag2);
         poseEstimate = Optional.of(estimate);
       }
       return poseEstimate.get().getPoseEstimate();
     }
 
-
   }
-
 
 }
